dfcf0237a5fbf2d8654750dac86ad28e4e5b14ab
DevonCurrent <dacurrent@bsu.edu>
2018-04-17 14:35:05 -0400

    Add TestAdminCommands that tests kick, ban, and create channels commands. Add testCreateChannels, which tests that two voice channels can be made. Fix TestGames, TicTacToe and Board classes to allow deployment of bot and testing of features.

--- src/commands/TicTacToe/Board.java
+++ src/commands/TicTacToe/Board.java
@@ -1,8 +1,5 @@
 package commands.TicTacToe;
 
-import net.dv8tion.jda.core.EmbedBuilder;
-import java.awt.*;
-
 // Board Sub-class
 public class Board{
     private int rows;
@@ -10,7 +7,7 @@ public class Board{
     private Piece[][] board;
     private int round;
 
-    public Board(int r, int c) {
+    Board(int r, int c) {
         rows = r;
         cols = c;
         board = new Piece[r][c];
@@ -25,20 +22,20 @@ public class Board{
         }
     }
 
-    void drawBoard() {
-        EmbedBuilder embedgame = new EmbedBuilder().setColor(Color.green).setTitle(Emoji.GAME + " Current Board (Round " + round + ")\n", null).setFooter(turn.getName() + " finished his/her turn.", null);
-        e.getChannel().sendMessage(embedgame.build()).queue();
-        round++;
-        StringBuilder line = new StringBuilder();
-
-        for (int i=0; i<rows; i++) {
-            for (int j=0; j<cols; j++) {
-                line.append(getEmojiPos(i, j)).append(" ");
-            }
-            line.append("\n");
-        }
-        e.getChannel().sendMessage(line.toString()).queue();
-    }
+//    void drawBoard() {
+//        EmbedBuilder embedgame = new EmbedBuilder().setColor(Color.green).setTitle(" Current Board (Round " + round + ")\n", null).setFooter(turn.getName() + " finished his/her turn.", null);
+//        e.getChannel().sendMessage(embedgame.build()).queue();
+//        round++;
+//        StringBuilder line = new StringBuilder();
+//
+//        for (int i=0; i<rows; i++) {
+//            for (int j=0; j<cols; j++) {
+//                line.append(getEmojiPos(i, j)).append(" ");
+//            }
+//            line.append("\n");
+//        }
+//        e.getChannel().sendMessage(line.toString()).queue();
+//    }
 
     void addPiece(Piece x, int r, int c) {
         board[r][c] = x;
@@ -51,7 +48,7 @@ public class Board{
     boolean isOccupied(int r, int c) {
         Piece p = board[r][c];
         String q = p.getID();
-        return !q.equals("	");
+        return q.equals("	");
     }
 
     void clearBoard() {
