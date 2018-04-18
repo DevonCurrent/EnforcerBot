e3e07f7e43e7236ded123c839afc4d970f88dee5
SMJonesPB <smjones9602@outlook.com>
2018-04-18 19:46:58 -0400

    Improved textchnnel assertion and occupancy

--- test/runOnDiscordTests/TestGames.java
+++ test/runOnDiscordTests/TestGames.java
@@ -1,12 +1,12 @@
 package runOnDiscordTests;
 
+import commands.TicTacToe.Board;
 import commands.TicTacToe.TicTacToe;
 import main.java.AccountCreator;
 import net.dv8tion.jda.core.JDA;
 import org.junit.Assert;
 import org.junit.Test;
 import commands.InviteCommand.SendClientMessage;
-
 
 public class TestGames {
     private AccountCreator accountCreator = new AccountCreator();
@@ -15,11 +15,16 @@ public class TestGames {
     @Test
     public void testTicTacToe(){
         JDA guild = null;
-        Assert.assertEquals(false, guild.getTextChannels());
-        TicTacToe game;
-        //Assert.assertEquals(true, game.startGame());
-        int[][] board = new int[3][3];
-        if (board[3][3] == 0){
+        try {
+            Assert.assertNull(guild.getTextChannels());
+        }
+        catch (NullPointerException NPE){
+            SendClientMessage sendMessage = new SendClientMessage();
+            sendMessage.sendMessageToDiscord(clientAccount, "There must not be a text channel.");
+        }
+
+        Board board = new Board(3, 3);
+        if (board.isOccupied(0, 0) || board.isOccupied(0, 1) || board.isOccupied(0, 2) || board.isOccupied(1, 0) || board.isOccupied(1, 1) || board.isOccupied(1, 2) || board.isOccupied(2, 0) || board.isOccupied(2, 1) || board.isOccupied(2, 2)){
             throw new RuntimeException("That spot is occupied.");
         }
     }
@@ -30,7 +35,7 @@ public class TestGames {
         sendMessage.sendMessageToDiscord(clientAccount, "!rng 10");
         String rngMessage = sendMessage.getMessage().getContentRaw();
         Assert.assertEquals(rngMessage, "!rng 10");
-        double random = Math.floor(Math.random() * 10) +1;
+        double random = Math.floor(Math.random() * 10) + 1;
         sendMessage.sendMessageToDiscord(clientAccount, "The random number generator generates " + random + ".");
     }
 }
